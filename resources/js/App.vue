3a0271222e1b3e54c06be8270aaa2feebcb3a461
codershisho <codershisho@gmail.com>
2024-01-09 16:46:23 +0000

    typescriptに切り替え wsl->devcontainer利用ならエラーにならない

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,12 @@
 <template>
-    <div class="page">
-        <p>{{ counter }}</p>
-        <button @click="counter += 1">click!</button>
-    </div>
+  <div class="page">
+    <p>{{ counter }}</p>
+    <button @click="counter += 1">click!</button>
+  </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            counter: 0,
-        };
-    },
-};
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref(0);
 </script>

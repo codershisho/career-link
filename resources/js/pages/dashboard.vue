fbb0e2a111c6e3bfcfacfb5d6d06ed4abbde128f
codershisho <codershisho@gmail.com>
2024-01-28 13:46:39 +0900

    tailwindo追加

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -4,6 +4,7 @@
   <v-btn color="secondary">aaa</v-btn>
   <v-btn color="info">aaa</v-btn>
   <v-btn color="accent">aaa</v-btn>
+  <span class="tw-text-3xl">aaa</span>
 </template>
 
 <script setup lang="ts"></script>
